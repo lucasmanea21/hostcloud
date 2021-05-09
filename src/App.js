48e5bb9f08b906a81d7c58f76e06a2c80fbffa9b
lucasmanea21 <lucasmanea@Yahoo.com>
2021-05-09 10:39:21 +0300

    finishing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import './App.css';
 import About from './pages/About';
 import Home from './pages/Home';
-import {Switch, Route } from 'react-router-dom'
+import {Switch, Route, useLocation} from 'react-router-dom'
 import Services from './pages/Services';
 import Contact from './pages/Contact';
+import {useEffect,Fragment} from 'react'
 
 function App() {
+  const ScrollToTop = () => {
+    const { pathname } = useLocation();
+  
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+  
+    return null;
+  }
+
   return (
+    <Fragment> 
+    <ScrollToTop />
     <Switch> 
       <Route path='/' exact component={Home} />
       <Route path='/about' component={About} />
       <Route path='/services' component={Services} />
       <Route path='/contact' component={Contact} />
     </Switch>
+    </Fragment>
   );
 }
 
